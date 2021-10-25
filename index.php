50f7932b5caa6d196a9f3d00ace424d572e4995f
Gabriel Rodrigues <88398438+GabrielRodriguesEtec@users.noreply.github.com>
2021-10-25 20:44:08 -0300

    correção

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Login. Controle de Estoque</title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <script src='assets/js/jquery-3.5.1.slim.min.js'></script>
-        <script src='assets/js/bootstrap.bundle.min.js'></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </head>
 
     <body>
